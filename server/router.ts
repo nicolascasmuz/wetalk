5efafeea428ee51b7c67f0ac128f066b4bc3c83b
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-02-08 15:11:28 -0300

    commit-013

--- server/router.ts
+++ server/router.ts
@@ -7,4 +7,5 @@ router.setRoutes([
   { path: "/", component: "signin-page" },
   { path: "/signin", component: "signin-page" },
   { path: "/chatroom", component: "chatroom-page" },
+  { path: "(.*)", redirect: "/home" },
 ]);
