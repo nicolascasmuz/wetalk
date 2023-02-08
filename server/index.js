8e894dbc600fef3c0cc4f6e38e99b44a86e45309
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-02-08 20:34:16 -0300

    commit-017

--- server/index.js
+++ server/index.js
@@ -137,7 +137,8 @@ app.delete("/deleteroom", (req, res) => {
         console.error("Error removing document: ", error);
     });
 });
-app.use(express.static(path.join(__dirname, "../dist")));
+/* app.use(express.static(path.join(__dirname, "../dist"))); */
+app.use(express.static("dist"));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
--- server/index.ts
+++ server/index.ts
@@ -150,7 +150,8 @@ app.delete("/deleteroom", (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "../dist")));
+/* app.use(express.static(path.join(__dirname, "../dist"))); */
+app.use(express.static("dist"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
